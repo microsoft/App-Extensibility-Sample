c8486a548ae3ca143b6521d2d439fb641d0c3703
Anir Agarwal <anaga@microsoft.com>
2016-03-28 10:11:59 -0700

    Commiting Default Closed Hamburger

--- ExtensibilitySample/EditorTab.xaml.cs
+++ ExtensibilitySample/EditorTab.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 using Windows.ApplicationModel.AppService;
+using Windows.UI.Xaml.Media.Imaging;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -90,8 +91,58 @@ namespace ExtensibilitySample
 
         }
 
-        private async void Crop_Click(object sender, RoutedEventArgs e)
+        private async void Grayscale_Click(object sender, RoutedEventArgs e)
         {
+            using (var connection = new AppServiceConnection())
+            {
+                connection.AppServiceName = "com.microsoft.grayscaleservice";
+                connection.PackageFamilyName = "AppExtensibility.Extension.Grayscale_byq669axdz8jy";
+
+                AppServiceConnectionStatus status = await connection.OpenAsync();
+                if (status == AppServiceConnectionStatus.Success)
+                {
+                    #region SendMessage
+                    // send request to service
+                    var request = new ValueSet();
+                    request.Add("Command", "Grayscale");
+                    request.Add("Pixels", ImageTools.GetBitmapBytes(AppData.currentImage));
+                    request.Add("Height", AppData.currentImage.PixelHeight);
+                    request.Add("Width", AppData.currentImage.PixelWidth);
+
+                    // get response
+                    AppServiceResponse response = await connection.SendMessageAsync(request);
+
+                    if (response.Status == AppServiceResponseStatus.Success)
+                    #endregion
+                    #region HandleMessage
+                    {
+                        #region ErrorHandling
+                        // convert imagestring back
+                        ValueSet message = response.Message as ValueSet;
+                        if (message.ContainsKey("Pixels") &&
+                            message.ContainsKey("Height") &&
+                            message.ContainsKey("Width"))
+                        {
+                            #endregion
+                            byte[] pixels = message["Pixels"] as byte[];
+                            int height = (int)message["Height"];
+                            int width = (int)message["Width"];
+
+                            // encode the bytes to a string, and then the image.
+                            string encodedImage = await ImageTools.EncodeBytesToPNGString(pixels, (uint)width, (uint)height);
+                            await AppData.currentImage.SetSourceAsync(ImageTools.DecodeStringToBitmapSource(encodedImage));
+                            AppData.currentImageString = encodedImage;
+                        }
+                    }
+                    #endregion
+
+                }
+            }
+        }
+
+        private async void Crop_Click(object sender, RoutedEventArgs e) 
+        {
+            #region File Setup
             // Load a File Picker that shows image file types
             FileOpenPicker open = new FileOpenPicker();
             open.FileTypeFilter.Add(".jpg");
@@ -112,15 +163,23 @@ namespace ExtensibilitySample
                     // Create a destination file
                     StorageFile dest = await KnownFolders.PicturesLibrary.CreateFileAsync("Cropped.jpg", CreationCollisionOption.ReplaceExisting);
 
+                    #endregion 
+
                     // Call CropImageAsync and receive Result
                     LaunchUriResult result = await this.CropImageAsync(source, dest, 500, 500);
 
+                    // Load Destination Image into Image Preview
+                    var stream = await dest.OpenReadAsync();
+                    await AppData.currentImage.SetSourceAsync(stream);
+
+            #region Error Handling
                     // Verify result and load picture into the source
                     if (result.Status == LaunchUriStatus.Success && result.Result != null)
                     {
                         string imgstr = await ImageTools.FileToString(dest);
                         if (imgstr != null)
                         {
+
                             await AppData.currentImage.SetSourceAsync(ImageTools.DecodeStringToBitmapSource(imgstr));
                             AppData.currentImageString = imgstr;
                         }
@@ -132,54 +191,10 @@ namespace ExtensibilitySample
                     MessageDialog md = new MessageDialog("Error loading image file.");
                     await md.ShowAsync();
                 }
+                
             }
+            #endregion
         }
-
-        private async void Grayscale_Click(object sender, RoutedEventArgs e)
-        {
-            using (var connection = new AppServiceConnection())
-            {
-                connection.AppServiceName = "com.microsoft.grayscaleservice";
-                connection.PackageFamilyName = "AppExtensibility.Extension.Grayscale_byq669axdz8jy";
-
-                AppServiceConnectionStatus status = await connection.OpenAsync();
-                if (status != AppServiceConnectionStatus.Success)
-                {
-                    Debug.WriteLine("Failed App Service Connection");
-                }
-                else
-                {
-                    // send request to service
-                    var request = new ValueSet();
-                    request.Add("Command", "Load");
-                    request.Add("Pixels", ImageTools.GetBitmapBytes(AppData.currentImage));
-                    request.Add("Height", AppData.currentImage.PixelHeight);
-                    request.Add("Width", AppData.currentImage.PixelWidth);
-
-                    // get response
-                    AppServiceResponse response = await connection.SendMessageAsync(request);
-                    if (response.Status == AppServiceResponseStatus.Success)
-                    {
-                        // convert imagestring back
-                        ValueSet message = response.Message as ValueSet;
-                        if (message.ContainsKey("Pixels") &&
-                            message.ContainsKey("Height") &&
-                            message.ContainsKey("Width"))
-                        {
-                            byte[] pixels = message["Pixels"] as byte[];
-                            int height = (int)message["Height"];
-                            int width = (int)message["Width"];
-
-                            // encode the bytes to a string, and then the image.
-                            string encodedImage = await ImageTools.EncodeBytesToPNGString(pixels, (uint)width, (uint)height);
-                            await AppData.currentImage.SetSourceAsync(ImageTools.DecodeStringToBitmapSource(encodedImage));
-                            AppData.currentImageString = encodedImage;
-                        }
-                    }
-                }
-            }
-        }
-
 
         private async Task<LaunchUriResult> CropImageAsync(IStorageFile input, IStorageFile destination, int width, int height)
         {
